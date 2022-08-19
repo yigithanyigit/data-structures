6ea80bd73c2831e43c02bf7eaf9b3a7edc705971
Yiğithan Yiğit <croizerxx@gmail.com>
2022-08-19 23:53:45 +0300

    implemented linear pobing completetly
    tested but not very detailed.

--- python/hashTableLinearProbing/main.py
+++ python/hashTableLinearProbing/main.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     table.insert("Deneme", 35);
     table.insert("Deneme", 65);
     table.insert("deneme", 45);
+    table.delete("Deneme");
+    table.insert("Deneme", 65);
     print(table.getTable(), table.getLoadFactor(), table.searchTable("Deneme"));
